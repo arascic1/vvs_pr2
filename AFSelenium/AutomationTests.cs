0a50995cff79f3c1589468e6d929485eaed91e69
Ahmed Rascic <arascic1@etf.unsa.ba>
2024-01-18 18:12:45 +0100

    init

---
+++ AFSelenium/AutomationTests.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using PageObjectModel.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Tests
+{
+    public class AutomationTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void InitScript()
+        {
+
+            _driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void LoginWithAlphanumericPassword()
+        {
+            _driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 10);
+
+            _driver.Navigate().GoToUrl("https://demoqa.com/books");
+            _driver.Manage().Window.Size = new System.Drawing.Size(900, 900);
+            _driver.FindElement(By.Id("login")).Click();
+            _driver.FindElement(By.Id("userName")).SendKeys("user");
+            _driver.FindElement(By.Id("password")).SendKeys("BxUSHkVj7QSh3yN!");
+            
+            _driver.FindElement(By.Id("newUser")).Click();
+
+            _driver.FindElement(By.Id("userName")).SendKeys("user");
+            _driver.FindElement(By.Id("firstname")).Click();
+            _driver.FindElement(By.Id("firstname")).SendKeys("user");
+            _driver.FindElement(By.Id("lastname")).SendKeys("user");
+            _driver.FindElement(By.Id("password")).SendKeys("5Bh24JidJyX6");
+
+            _driver.SwitchTo().Frame(0);
+
+            _driver.FindElement(By.CssSelector(".recaptcha-checkbox-border")).Click();
+            _driver.SwitchTo().DefaultContent();
+            ((IJavaScriptExecutor)_driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+
+            _driver.FindElement(By.Id("register")).Click();
+
+            // ovaj alert se NEĆE pojaviti i test će PASTI jer je u pitanju BUG
+            _driver.SwitchTo().Alert().Accept();
+
+            _driver.Close();
+        }
+
+        [Test]
+        public void CannotAddAlreadyAddedBook()
+        {
+            _driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 10);
+
+            _driver.Navigate().GoToUrl("https://demoqa.com/books");
+            _driver.Manage().Window.Maximize();
+            _driver.FindElement(By.Id("login")).Click();
+            _driver.FindElement(By.Id("userName")).SendKeys("user");
+            _driver.FindElement(By.Id("password")).SendKeys("BxUSHkVj7QSh3yN!");
+            _driver.FindElement(By.Id("login")).Click();
+
+            _driver.FindElement(By.LinkText("Git Pocket Guide")).Click();
+            ((IJavaScriptExecutor)_driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+            _driver.FindElement(By.CssSelector(".text-right > #addNewRecordButton")).Click();
+            
+            Thread.Sleep(500);
+
+            Assert.That(_driver.SwitchTo().Alert().Text, Is.EqualTo("Book added to your collection."));
+            _driver.SwitchTo().Alert().Accept();
+
+            _driver.FindElement(By.CssSelector(".col-md-6")).Click();
+
+            // knjiga je dodana, dugme ne treba biti vidljivo, test PADA jer je u pitanju BUG
+
+            var btn = _driver.FindElement(By.Id("addNewRecordButton"));
+            Assert.IsFalse(btn.Displayed);
+
+            _driver.Close();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
---
+++ AFSelenium/HomePage.cs
@@ -0,0 +1,28 @@
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace PageObjectModel.Source.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.Name, Using = "q")]
+        private IWebElement _searchtxtbox;
+
+        [FindsBy(How = How.Name, Using = "btnK")]
+        private IWebElement _searchbtn;
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Search(string searchText)
+        {
+            _searchtxtbox.SendKeys(searchText);
+            _searchtxtbox.SendKeys("\n");
+        }
+    }
+}
